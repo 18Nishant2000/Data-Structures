c9d685b18bbc18ad9686ef98846ae080504c10b3
Nishant Bansal <bansal.nishant18052000@gmail.com>
2019-10-27 13:13:55 +0530

    Static QUEUE

---
+++ STATICQU.C
@@ -0,0 +1,57 @@
+#define size 5
+void main()
+{
+int QUEUE[size],front=-1,rear=-1,choice,times,i;
+clrscr();
+printf("\nEnter number of times you want to execute the program: ");
+scanf("%d",&times);
+for(i=0;i<times;i++)
+{
+printf("\n\nMENU:\n\n1:Insertion\n\n2:Deletion\n\n3:Display\n\n4:Exit\n\nEnter your choice: ");
+scanf("%d",&choice);
+if(choice==1)
+{
+int element;
+printf("\n\nEnter an element: ");
+scanf("%d",&element);
+if(rear==size-1)
+printf("\nOverflow");
+else if(front==-1 && rear==-1)
+{
+front++;
+rear++;
+}
+else
+rear++;
+QUEUE[rear]=element;
+printf("\n\nElement inserted successfully...");
+}
+else if(choice==2)
+{
+if(front==-1)
+printf("\nUnderflow");
+else
+{
+printf("\n\nDeleted element is %d.",QUEUE[front]);
+if(front==rear)
+{
+front=-1;
+rear=-1;
+}
+else
+front++;
+}
+}
+else if(choice==3)
+{
+for(i=front;i<=rear;i++)
+printf("%d ",QUEUE[i]);
+getch();
+}
+else if(choice==4)
+exit(0);
+else
+printf("\n\nWrong Input!!!");
+}
+getch();
+}
